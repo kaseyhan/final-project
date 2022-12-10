10080287c9bce3d453afab62ecb3e985a1591586
Gagan Kadadevarmath <gagan2@illinois.edu>
2022-12-10 00:07:13 -0600

    finished home page with no api implementation

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,23 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
-import { Checkbox, Card, Typography } from '@mui/material'
-
+import { FaPlus } from 'react-icons/fa'
+import { Checkbox, Card, Typography, CircularProgress, IconButton } from '@mui/material'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import styles from '../styles/Home.module.css'
+import { FaCreativeCommonsSamplingPlus } from 'react-icons/fa'
+import AnnouncementModal from '../components/AnnouncementModal'
+
+const BASE_URL = 'https://gsk-final-project-api.herokuapp.com/api/';
+const API = axios.create({ baseURL: BASE_URL });
 
 export default function Home() {
   const tasks = [
     {
       'id': '1',
-      'title': 'Do the dishes'
+      'title': 'Clean the dishes'
     },
     {
       'id': '2',
       'title': 'Take out the trash'
-    },
-    {
-      'id': '3',
-      'title': 'Get the mail'
     },
   ]
 
@@ -41,6 +44,15 @@ export default function Home() {
       'id': '2',
       'title': 'Bar Crawl'
     },
+    {
+      'id': '3',
+      'title': 'Dinner'
+    },
+    {
+      'id': '4',
+      'title': 'Bar Crawl'
+    },
+
   ]
 
   const ItemCard = ({ title, isTask }) => {
@@ -51,13 +63,72 @@ export default function Home() {
         flexDirection: 'row',
         padding: '0 5px',
         margin: 0,
-        width: '100%'
+        width: '100%',
+        border: '2px black solid'
       }}>
         <p>{title}</p>
         {isTask && <Checkbox />}
       </Card>
     );
   };
+
+  const [toDoData, setToDoData] = useState(null);
+  const [announcementsData, setAnnouncementsData] = useState(null);
+  const [eventsData, setEventsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(true);
+  const [newAnnouncement, setNewAnnouncement] = useState('');
+
+  const fetchData = async () => {
+    try {
+      // const res = await API.get('users');
+      // console.log(res.data.data);
+      // axios.get('/api/endpoint1')
+      //   .then(response => setToDoData(response.data))
+      //   .catch(error => console.error(error));
+
+      // axios.get('/api/endpoint2')
+      //   .then(response => setAnnouncementsData(response.data))
+      //   .catch(error => console.error(error));
+
+      // axios.get('/api/endpoint2')
+      //   .then(response => setEventsData(response.data))
+      //   .catch(error => console.error(error));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetchData();
+
+    setToDoData(tasks);
+    setAnnouncementsData(announcements);
+    setEventsData(upcomingEvents);
+    setIsLoading(false);
+    console.log(newAnnouncement)
+  }, []);
+
+  const openModal = () => {
+    setShowModal(true);
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
+  const handleInput = event => {
+    setNewAnnouncement(event.target.value)
+  }
+
+  const updateAnnouncementData = data => {
+    console.log(data)
+    setNewAnnouncement(data);
+    let newAnnouncementData = {'id': 3, 'title':newAnnouncement}
+    setAnnouncementsData([...announcementsData, newAnnouncementData]);
+  }
 
   return (
     <div className={styles.container}>
@@ -67,36 +138,60 @@ export default function Home() {
           Welcome, Name
         </div>
 
-        <div className={styles.dashboard}>
-          <div className={styles.left}>
-            <div className={styles.toDoContainer}>
-              <Typography style={{ fontSize: '36px' }}>Your To-Do's</Typography>
-              <div className={styles.list}>
-                {tasks && (tasks.map((task) => (
-                  <ItemCard key={task.id} title={task.title} isTask />
-                )))}
+        {isLoading ? <CircularProgress style={{ marginTop: '100px' }} /> :
+          <div className={styles.dashboard}>
+            <div className={styles.left}>
+              <div className={styles.toDoContainer}>
+                <Typography style={{ fontSize: '36px' }}>Your To-Do's</Typography>
+                <div className={styles.list}>
+                  <div className={styles.listContent}>
+                    {(toDoData && toDoData.length > 0) ? (toDoData.map((task) => (
+                      <ItemCard key={task.id} title={task.title} isTask />
+                    ))) : <h3>No Tasks...</h3>}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.right}>
+              <div className={styles.announcementsContainer}>
+                <Typography style={{ fontSize: '24px' }}>Announcements</Typography>
+                <IconButton onClick={openModal} style={{ position: 'absolute', top: 0, right: 0, padding: 0, margin: '7px' }}>
+                  <FaPlus size={25} />
+                </IconButton>
+
+                {showModal && (
+                  <AnnouncementModal
+                    showModal={showModal}
+                    newAnnouncement={newAnnouncement}
+                    updateAnnouncementData={updateAnnouncementData}
+                    closeModal={closeModal}
+                    handleInput={handleInput}
+                  />
+                )}
+
+                <div className={styles.list}>
+                  <div className={styles.listContent}>
+                    {(announcementsData && announcementsData.length > 0) ? (announcementsData.map((announcement) => (
+                      <ItemCard key={announcement.id} title={announcement.title} />
+                    ))) : <h3>No Announcements...</h3>}
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.eventsContainer}>
+                <Typography style={{ fontSize: '24px' }}>Upcoming Events</Typography>
+                <div className={styles.list}>
+                  <div className={styles.listContent}>
+                    {(eventsData && eventsData.length > 0) ? (eventsData.map((event) => (
+                      <ItemCard key={event.id} title={event.title} />
+                    ))) : <h3>No Upcoming Events...</h3>}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className={styles.right}>
-            <div className={styles.announcementsContainer}>
-              <Typography style={{ fontSize: '24px' }}>Announcements</Typography>
-              <div className={styles.list}>
-                {announcements && (announcements.map((announcement) => (
-                  <ItemCard key={announcement.id} title={announcement.title} />
-                )))}
-              </div>
-            </div>
-            <div className={styles.eventsContainer}>
-              <Typography style={{ fontSize: '24px' }}>Upcoming Events</Typography>
-              <div className={styles.list}>
-                {upcomingEvents && (upcomingEvents.map((event) => (
-                  <ItemCard key={event.id} title={event.title} />
-                )))}
-              </div>
-            </div>
-          </div>
-        </div>
+        }
       </div>
     </div >
   )
