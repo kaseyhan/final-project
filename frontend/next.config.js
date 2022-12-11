efd6858f0ba94427bf2279906f3c7feb395308ff
Gagan Kadadevarmath <gagan2@illinois.edu>
2022-12-11 03:15:02 -0600

    readying frontend for deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/<GITLAB_PROJECT_NAME>' : '',
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '/'
+  }
 }
 
 module.exports = nextConfig
