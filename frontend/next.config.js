889507adc0ac2d4c19ba9f512114d845011611db
Gagan Kadadevarmath <gagan2@illinois.edu>
2022-12-11 12:07:52 -0600

    readying frontend for deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/final-project' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/final-project/frontend/' : '',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     loader: 'akamai',
